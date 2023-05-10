cd660b7ef691355e395801177d701af6178166d7
JJ Merelo <jjmerelo@gmail.com>
2023-05-10 12:12:08 +0200

    Making the thing work

--- scripts/track-ship.js
+++ scripts/track-ship.js
@@ -1,7 +1,5 @@
-import AISStreamer from "../lib/index.js
-"
+import { AISstreamer } from "../lib/index.js";
 const API_KEY = process.env.AISSTREAM_API_KEY;
 const SHIP_NAME = process.env.SHIP_NAME.toUpperCase();
 
-
-const tracker = new AISStreamer( API_KEY, SHIP_NAME)
+const tracker = new AISstreamer(API_KEY, SHIP_NAME);
