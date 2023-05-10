b79bf7f1971b2791693d4de9e45bb0b315088f4f
JJ Merelo <jjmerelo@gmail.com>
2023-05-10 13:52:05 +0200

    Improving docs for release

--- scripts/track-ship.js
+++ scripts/track-ship.js
@@ -1,5 +1,8 @@
 import { AIStrack } from "../lib/index.js";
+/** API_KEY needs to be defined as an environment variable */
 const API_KEY = process.env.AISSTREAM_API_KEY;
+
+/** The exact ship name needs to be used; this might include the company name */
 const SHIP_NAME = process.env.SHIP_NAME.toUpperCase();
 
 AIStrack(API_KEY, SHIP_NAME);
