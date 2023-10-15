83f20c8b20a399b3fa52160c123dc85bd6a75c9b
JJ Merelo <jjmerelo@gmail.com>
2023-10-15 18:27:42 +0200

    :sparkles: #20 failed example that would obtain the MMSI

---
+++ examples/get_MMSI_for_name.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import {load} from 'cheerio';
+
+async function getMMSIsForShipName(shipName) {    
+    const response = await axios.get('https://www.itu.int/mmsapp/ShipStation/list');
+    const $ = load(response.data);
+
+    // Submit the ship name
+    $('input[name="shipName"]').val(shipName);
+    $('form').submit();
+
+    const mmsis = [];
+
+    // Find all rows in the table
+    const rows = $('table tr.odd, table tr.even');
+
+    rows.each((i, row) => {
+        const columns = $(row).find('td');
+
+        // Check if the ship name matches
+        if ($(columns[1]).text().trim() === shipName) {
+            // Extract the MMSI and store it in the array
+            const mmsi = $(columns[0]).text().trim();
+            mmsis.push(mmsi);
+        }
+    });
+
+    return mmsis;
+}
+
+// Example usage
+getMMSIsForShipName('Titanic').then(console.log);
