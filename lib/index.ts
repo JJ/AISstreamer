771f6a78486c1b39ccfc4cdeb8868716f7eab16e
JJ Merelo <jjmerelo@gmail.com>
2023-10-16 20:29:15 +0200

    Restoring and fixing #21 :sparkles:
    Thanks to https://stackoverflow.com/questions/77303916/how-can-you-read-a-blob-in-deno/77303963\#77303963

---
+++ lib/index.ts
@@ -0,0 +1,46 @@
+/**
+ * URL used by the API; this might change if the version changes or at any time, since it's experimental
+ */
+const AIS_API_URL = "wss://stream.aisstream.io/v0/stream";
+
+export const defaultBoundingBox = [
+  [
+    [-90, -180],
+    [90, 180],
+  ],
+];
+
+let socket = null;
+
+// Creates a deno websocket client and subscribes to the AIS API
+export function createSocket(apiKey, boundingBoxes = defaultBoundingBox) {
+  if (socket) {
+    return socket;
+  }
+  socket = new WebSocket(AIS_API_URL);
+
+  socket.onopen = () => {
+    console.log("Socket connected");
+    socket.send(
+      JSON.stringify({
+        apiKey,
+        boundingBoxes,
+      })
+    );
+  };
+
+  socket.onmessage = (event) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const decodedBlob = reader.result;
+      const message = JSON.parse(decodedBlob);
+      console.log(message);
+    };
+    reader.readAsText(event.data);
+  };
+
+  socket.onclose = () => {
+    console.log("Socket closed");
+  };
+  return socket;
+}
