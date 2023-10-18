ed2c7fb075f7e53e88fde62f57e0df573f79fbb2
JJ Merelo <jjmerelo@gmail.com>
2023-10-18 10:10:19 +0200

    Checkpoint for stackoverflow question

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -1,39 +1,56 @@
 import { readFileSync } from "fs";
-import path from "path";
-import { Server } from "mock-socket";
-import { AIS_API_URL, AIStrackAll } from "../lib/index.js";
-import { strictEqual } from "node:assert";
+import { WebSocket, Server } from "mock-socket";
+// import { AIS_API_URL, AIStrackAll, defaultBoundingBox } from "../lib/index.js";
+// import { strictEqual } from "node:assert";
 
 describe("socket mock test", () => {
   let server;
+  let socket;
+
+  let count = 0;
+  function callback(message) {
+    count++;
+    if (count === 5) {
+      socket.close();
+    }
+    console.log(message, count);
+  }
 
   before(() => {
     server = new Server(AIS_API_URL);
-
     const messageExamples = JSON.parse(
-      readFileSync(path.join(__dirname, "message_examples.json"), "utf8")
+      readFileSync("tests/message_examples.json")
     );
 
     server.on("connection", (socket) => {
       const messageType = Object.keys(messageExamples);
       const message =
-        obj[messageType[(messageType.length * Math.random()) << 0]];
+        messageExamples[messageType[(messageType.length * Math.random()) << 0]];
       socket.send(JSON.stringify(message));
+    });
+
+    socket = new WebSocket(AIS_API_URL);
+    socket.addEventListener("open", (_) => {
+      const subscriptionMessage = {
+        APIkey: "API_KEY",
+        BoundingBoxes: defaultBoundingBox,
+      };
+      socket.send(JSON.stringify(subscriptionMessage));
+    });
+    socket.addEventListener("error", (event) => {
+      console.error(event);
+    });
+    socket.addEventListener("message", (event) => {
+      const aisMessage = JSON.parse(event.data);
+      callback(aisMessage);
+    });
+    socket.addEventListener("close", () => {
+      it("should have sent 5 messages", (done) => {
+        strictEqual(count, 5);
+        done();
+      });
     });
   });
 
-  let count = 0;
-  function callback(message) {
-    console.log(message);
-    count++;
-    if (count === 5) {
-      server.close();
-    }
-  }
-  AIStrackAll("API_KEY", callback);
-
-  it("should have sent 5 messages", (done) => {
-    strictEqual(count, 5);
-    done();
-  });
+  //  AIStrackAll("API_KEY", defaultBoundingBox, callback, server);
 });
